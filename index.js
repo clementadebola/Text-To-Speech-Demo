095642c136638ada04d88fc09c8edf05e863bb80
clement <clementadebola54@gmail.com>
2024-01-17 23:01:32 +0100

    new update

--- index.js
+++ index.js
@@ -7,17 +7,34 @@
 function speak() {
     const textToRead  = document.getElementById('text-to-read').value;
 
+    // const downloadButton = document.getElementById('downloadButton');
+
     if ('speechSyntheis' in window) {
         const synthesis = window.speechSyntheis;
         const utterance = new SpeechSynthesisUtterance(textToRead);
 
         synthesis.speak(utterance);
+        
     } else {
         alert('your browser does not support the web Speech API.');
 
     }
+
+
+
+    // const downloadAudio = () => {
+    //     const utterance = new SpeechSynthesisUtterance(textToRead.value);
+    //     const blob = new Blob([textToRead.value], {type: 'text/plain'});
+    //     const url = URL.createObjectURL(blob);
+    //     const a = document.createElement('a');
+    //     a.href = url;
+    //     a.download = 'text'
+    //     a.click();
+    //     document.body.appendChild(a);
+
+    // }
 }
 
 //use 
 
-document.getElementById('speakbutton').addEventListener('click', speak)
+document.getElementById('speakbutton').addEventListener('click', speak);
